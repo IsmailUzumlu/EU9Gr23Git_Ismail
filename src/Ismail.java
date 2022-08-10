5dcbbcfa36a480d0fe832263fe5823fbeaa85b23
ismog <ismograpy@gmail.com>
2022-08-10 20:56:05 +0100

    second commit from ismail

--- src/Ismail.java
+++ src/Ismail.java
@@ -1,5 +1,7 @@
 public class Ismail {
     public static void main(String[] args) {
         System.out.println("first change from ismail");
+        System.out.println("first change from ismail");
+
     }
 }
